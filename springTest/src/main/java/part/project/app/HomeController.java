7a258b19bd7554335aea0e123bd02feb65da5afb
woo289 <rhddbgkqtlek@naver.com>
2019-12-01 13:25:52 +0900

    하핫

---
+++ springTest/src/main/java/part/project/app/HomeController.java
@@ -0,0 +1,47 @@
+package part.project.app;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		String id = "¼½½º";
+		
+		logger.trace("trace, id : {}", id);
+		logger.debug("debug, id : {}", id);
+		logger.info("info, id : {}", id);
+		logger.warn("warn, id : {}", id);
+		logger.error("error, id : {}", id);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
